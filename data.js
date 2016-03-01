20e00e57e528fbe0a92b90789c850cff58a83016
Hyun <lhc4128@gmail.com>
2016-03-01 14:51:28 -0800

    Created a basic layout for search page with html and css.

---
+++ data.js
@@ -0,0 +1,30 @@
+search, distance, list, review, comment
+
+search {
+  address:
+  city:
+  zipcode:
+  location:
+};
+
+distance {
+
+};
+
+list {
+  name:
+  star:
+  preview:
+  image:
+  time:
+}
+
+review {
+  store:
+  address:
+  star:
+  title
+  text:
+  picture:
+  map
+}
