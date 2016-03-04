acf570878ebb395b8444fecad4e44c134aa0f44f
Hyun <lhc4128@gmail.com>
2016-03-04 15:08:41 -0800

    Post button will now add a new review into an array of objects called 'reviews' and will display it on the page. I still need to make function to hide the writeReview panel when post button is clicked. #3

--- js/default.js
+++ js/default.js
@@ -46,16 +46,6 @@ var reviewLocation = document.getElementById('reviewList');
 var searchInput = document.getElementById('keyword');
 var lookUp = document.getElementById('search');
 
-// var remove = document.getElementsByClassName('panel panel-default');
-// var parentRemove = remove.parentNode;
-//
-// function removeResults(event) {
-//   event.preventDefault();
-//   for (i = 0; i < remove.length; i++)
-//     parentRemove.removeChild(remove[i]);
-// }
-// This is an enhancement to the issue #1. Will comabeck later to finish if time allows.
-
 function results(event) {
   event.preventDefault();
   for (var i = 0; i < reviews.length; i++) {
@@ -127,10 +117,97 @@ function results(event) {
   }
 }
 
-// lookUp.addEventListener('submit', removeResults);
-//Enhancement to the search function. Will visit later.
 lookUp.addEventListener('submit', results);
 // end of issue 1 & 2.
 
+// var remove = document.getElementsByClassName('panel panel-default');
+// var parentRemove = remove.parentNode;
+//
+// function removeResults(event) {
+//   event.preventDefault();
+//   for (i = 0; i < remove.length; i++)
+//     parentRemove.removeChild(remove[i]);
+// }
+//
+// lookUp.addEventListener('submit', removeResults);
+//This is an Enhancement to the search function. Will visit later.
+
 //issue 3. User can post a review.
+//hide&show function for write button.
 var writeReview = document.getElementById('write');
+var reviewInput = document.getElementById('reviewPanel');
+
+function toggleClass(value, element) {
+  var reviewClasses = element.className.split(' ');
+  var position = reviewClasses.indexOf(value);
+
+  if (position == -1) {
+    reviewClasses.push(value);
+  } else {
+    reviewClasses.splice(position, 1);
+  }
+
+  element.className = reviewClasses.join(' ');
+}
+
+writeReview.addEventListener('click', function() {
+  toggleClass('hidden', reviewInput);
+});
+
+//post function.
+var postReview = document.getElementById('post');
+
+function post() {
+  event.preventDefault();
+  //create a new object with input values and push it to an array 'reviews'.
+  var reviewName = document.getElementById('name');
+  var reviewRestaurant = document.getElementById('restaurant');
+  var reviewAddress = document.getElementById('address');
+  var reviewCity = document.getElementById('city');
+  var reviewState = document.getElementById('state');
+  var reviewZip = document.getElementById('zip');
+  var reviewComment = document.getElementById('comment');
+
+  var newReview = {};
+  newReview.name = reviewName.value;
+  newReview.restaurant = reviewRestaurant.value;
+  newReview.address = reviewAddress.value + ' ' + reviewCity.value + ' ' + reviewState.value + ' ' + reviewZip.value;
+  newReview.city = reviewCity.value;
+  newReview.zip = reviewZip.value;
+  newReview.comment = reviewComment.value;
+
+  reviews.unshift(newReview);
+
+  //append the values of 'newReview' to the list of reviews.
+  var reviewContent = document.createElement('div')
+  reviewContent.setAttribute('class', 'panel panel-default');
+
+  var commentHeading = document.createElement('div');
+  commentHeading.setAttribute('class', 'panel-heading');
+
+  var profilePicture = document.createElement('img');
+  profilePicture.setAttribute('src', 'images/logo.png');
+  profilePicture.setAttribute('width', '50px');
+  profilePicture.setAttribute('height', '50px');
+
+  var profileName = document.createElement('span');
+  profileName.setAttribute('class', 'panel-title');
+
+  var userName = document.createTextNode(reviews[0].name);
+
+  var commentBody = document.createElement('div');
+  commentBody.setAttribute('class', 'panel-body');
+
+  var commentText = document.createTextNode(reviews[0].comment);
+
+  profileName.appendChild(userName);
+  commentHeading.appendChild(profilePicture);
+  commentHeading.appendChild(profileName);
+  commentBody.appendChild(commentText);
+  reviewContent.appendChild(commentHeading);
+  reviewContent.appendChild(commentBody);
+  reviewLocation.appendChild(reviewContent);
+}
+
+
+postReview.addEventListener('click', post)
